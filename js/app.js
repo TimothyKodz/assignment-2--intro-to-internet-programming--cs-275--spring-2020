579fc48d89c0f48f652093b25be37ae05ce1b96b
TimothyKodz <55055311+TimothyKodz@users.noreply.github.com>
2020-04-27 17:30:37 -0400

    added div class directions for styling

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function getRecipe() {
             <label for="rice-val">Enter amount of rice you would like to cook</label>
             <input type="number" id="rice-val" step="any" oninput="calcRatios()">
 
+            <div class="directions">
             <p>For slightly al dente rice:</p>
             <ol>
                 <li>Combine <span id="c-rice-val">0</span> cups of rice with <span id="c-water-val">0</span> cups of water or broth and <span id="c-oil-val">0</span> Tbsp olive oil.</li>
@@ -21,9 +22,12 @@ function getRecipe() {
                 <li>Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.</li>
                 <li>Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.</li>
             </ol>
+            </div>
 
+            <div class="directions">
             <p>For softer rice:</p>
-            <p>Increase liquid by 1/2 cup and cook time by 5 minutes.</p>`);
+            <p class="extra-d">Increase liquid by 1/2 cup and cook time by 5 minutes.</p>
+            </div>`);
     }
     else if (riceType == `w-rice`) {
         display.innerHTML = (
@@ -32,11 +36,13 @@ function getRecipe() {
             <label for="rice-val">Enter amount of rice you would like to cook</label>
             <input type="number" id="rice-val" step="any" oninput="calcRatios()">
 
+            <div class="directions">
             <ol>
                 <li>Combine <span id="w-rice-val">0</span> cup of rice with <span id="w-water-val">0</span> cups of water and <span id="w-oil-val">0</span> Tbsp olive oil.</li>
                 <li>Bring to a boil, then reduce heat to the lowest setting.</li>
                 <li>Cook for about 18 minutes.</li>
-            </ol>`);
+            </ol>
+            </div>`);
     }
 }
 
